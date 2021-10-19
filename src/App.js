c8923900ead8d71b4f0ec8690b7bd93831be41d2
asaf <asafalmog119@gmail.com>
2021-10-19 22:06:07 +0300

    update css

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ function App() {
 
           <MDBCol sm='12' lg='8'>
 
-            <div style={{ width: '100%', height: '80vh' }}>
+            <div id='mapDiv'>
               <WrappedMap
                 googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_KEY}&v=3.exp&libraries=geometry,drawing,places`}
                 loadingElement={<div style={{ height: '100%' }} />}
